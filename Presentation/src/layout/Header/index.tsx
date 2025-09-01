9cd0e0e2d3e9b75b66c36f3f196c15d665a9251a
Alexander Groshev <gav.groshev@gmail.com>
2025-09-01 21:37:43 +0300

    Fixed header layout in mobile version

--- Presentation/src/layout/Header/index.tsx
+++ Presentation/src/layout/Header/index.tsx
@@ -11,9 +11,19 @@ export const Header: React.FC<HeaderProps> = ({ toggleSidebar }) => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <header className="d-flex justify-content-between border-bottom align-items-center bg-body w-100 px-3 py-2">
+<header className="border-bottom bg-body w-100 px-3 py-2">
+  <div className="d-flex flex-column flex-md-row justify-content-center justify-content-md-between align-items-center">
+    {/* Левая часть */}
+    <div className="mb-2 mb-md-0">
       <HeaderLeft toggleSidebar={toggleSidebar} />
+    </div>
+
+    {/* Правая часть */}
+    <div>
       <HeaderRight theme={theme} toggleTheme={toggleTheme} />
-    </header>
+    </div>
+  </div>
+</header>
+
   );
 };
