5f8d8000ee4b73eb14567ec2f7a3bfcb77a6351f
Alexander Groshev <gav.groshev@gmail.com>
2025-09-02 00:00:51 +0300

    Github pages config

--- Presentation/vite.config.ts
+++ Presentation/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dyxboard/', // Замените 'repository-name' на имя вашего репозитория
 })
