3b1e8d3b4e95c0b63a83f81e3ac49ca85213b306
Alexander Groshev <gav.groshev@gmail.com>
2025-09-14 17:25:03 +0300

    Deleted bootstrap vendor folder

--- Presentation/src/App.tsx
+++ Presentation/src/App.tsx
@@ -1,9 +1,8 @@
 // App.tsx
-import "./assets/vendor/bootstrap/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/fonts/fonts.css";
 import "./assets/css/main.css";
 import "./assets/css/theme.css";
-import "./assets/vendor/bootstrap/js/bootstrap.bundle.min.js";
 
 import { AppRoutes } from "./routes/AppRoutes";
 import { FaviconUpdater } from "./shared/hooks/useFavicon.js";
